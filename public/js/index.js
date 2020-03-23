dccf75f8646c2e2690fd4e25cf000f9d60f83f16
Chidiebere <onyekachisamuel18@gmail.com>
2020-03-23 17:08:57 +0100

    fixed compare password

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 $('#user').on('click',(e)=>{
         $('#nav-profile').toggle()
 })
+$('#regBtn').on('click',(e)=>{
+        if($('#password').val()!==$('#confirmPassword').val()){
+                e.preventDefault()
+                $('.err').show()
 
+                console.log('check password')
+        }
+})
+const checkPassword =()=>{
+        if($('#password').val()!==$('#confirmPassword').val()){
+                $('#good').hide()
+                $('#bad').show();
+        }else{
+                $('.err').hide()
+                $('#bad').hide()
+                $('#good').show()
+        }
+}
+$('#confirmPassword').on('input',checkPassword)
+$('#password').on('input',checkPassword)
 
